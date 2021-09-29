bbdda7e7c2129766441107eb7d4daedf11667d97
Nischal Shrestha <nischalstha9@gmail.com>
2021-09-29 17:41:25 +0545

    database from php all in labn

--- database/colz_db.sql
+++ database/colz_db.sql
@@ -11,27 +11,27 @@ country VARCHAR(50)
 );
 
 INSERT INTO customer (customer_name,contact_name, address, city, postal_code, country) VALUES 
-("nischal", "9823000133", "Opera", "Sydney", "144660", "Australia");
+('nischal', '9823000133', 'Opera', 'Sydney', '144660', 'Australia');
 
 INSERT INTO customer (customer_name, contact_name, address, city, postal_code, country) VALUES
-("vivek", "9823000123", "banasthali", "Kathmandu", "133660", "Nepal"),
-("Anish", "9080000112", "machapokhari", "Kathmandu", "144330", "Nepal");
+('vivek', '9823000123', 'banasthali', 'Kathmandu', '133660', 'Nepal'),
+('Anish', '9080000112', 'machapokhari', 'Kathmandu', '144330', 'Nepal');
 
 INSERT INTO customer (customer_name, contact_name, address, city, postal_code, country) VALUES
-("kumar", "97265432", "jughu", "Delhi", "144330", "India"),
-("amit", "97365432", "jughu", "Delhi", "144330", "India"),
-("sugam", "97165432", "jughu", "Delhi", "144330", "India");
+('kumar', '97265432', 'jughu', 'Delhi', '144330', 'India'),
+('amit', '97365432', 'jughu', 'Delhi', '144330', 'India'),
+('sugam', '97165432', 'jughu', 'Delhi', '144330', 'India');
 
 INSERT INTO customer (customer_name, contact_name, address, city, postal_code, country) VALUES
-("kumar", "98492341", "minTown", "Shanghai", "13430", "China"),
-("amit", "98292341", "minTown", "Shanghai", "13430", "China"),
-("asis", "98192341", "minTown", "Shanghai", "13430", "China"),
-("sugam", "98792341", "minTown", "Shanghai", "13430", "China");
+('kumar', '98492341', 'minTown', 'Shanghai', '13430', 'China'),
+('amit', '98292341', 'minTown', 'Shanghai', '13430', 'China'),
+('asis', '98192341', 'minTown', 'Shanghai', '13430', 'China'),
+('sugam', '98792341', 'minTown', 'Shanghai', '13430', 'China');
 
-UPDATE customer SET city="Lalitpur" WHERE city="Kathmandu";
+UPDATE customer SET city='Lalitpur' WHERE city='Kathmandu';
 
-UPDATE customer SET city="New York", country="USA" WHERE city="Shanghai" AND country="China";
+UPDATE customer SET city='New York', country='USA' WHERE city='Shanghai' AND country='China';
 
-DELETE FROM customer WHERE country="Australia";
+DELETE FROM customer WHERE country='Australia';
 
-SELECT * FROM customer WHERE country in ("Nepal","India");
+SELECT * FROM customer WHERE country in ('Nepal','India');
